23eb095bdd4b0d7e18b7ecdec31eb7b0a9aac1cc
Manuel Herzog <manuel.herzog@esentri.com>
2021-02-11 17:37:31 +0100

    add clojure integration

--- src/main/java/com/esentri/order/excel/reader/OrderExcelReaderApplication.java
+++ src/main/java/com/esentri/order/excel/reader/OrderExcelReaderApplication.java
@@ -3,6 +3,7 @@ package com.esentri.order.excel.reader;
 import org.apache.commons.cli.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import com.esentri.clojure.order.excel.reader;
 
 public final class OrderExcelReaderApplication {
 
@@ -23,6 +24,12 @@ public final class OrderExcelReaderApplication {
 
         final long end = System.nanoTime();
         logger.info("Java Total in Nanoseconds: {}", end - start);
+
+        logger.info("Clojure run");
+        final long cljstart = System.nanoTime();
+        reader.execute(filePath, "output-clojure.xlsx");
+        final long cljstop = System.nanoTime();
+        logger.info("Total: {}", cljstop - cljstart);
     }
 
 }
